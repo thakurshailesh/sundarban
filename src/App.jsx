f353d6e284733a66b9b42b47c1d369d8d10c73ad
pinkijha <pinkijha1310@gmail.com>
2025-03-27 02:12:13 +0530

    setup using vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <h1 className="text-4xl text-center mt-10 text-blue-500">Welcome to Sundarban</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+      <nav className="bg-blue-500 p-4">
+        <h1 className="text-white text-2xl">Sundarban</h1>
+      </nav>
+    );
+  }
+  
